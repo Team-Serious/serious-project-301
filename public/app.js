8691784b7a8055395f3024ebd1fb2bd3d41827c5
rowaidsayyed <rowaidsayyed@gmail.com>
2020-04-18 19:13:06 +0300

    some css

--- public/app.js
+++ public/app.js
@@ -5,4 +5,19 @@ $(document).ready(function () {
     $('.dropdown-menu').toggleClass('hidden');
     $('.arrow').toggleClass('up');
   });
+
+  $('.RowaidInfo').hover(function(){
+    $('.Rowaid').toggleClass('animate');
+  });
+  $('.HussineInfo').hover(function(){
+    $('.Hussine').toggleClass('animate');
+  });
+  $('.Batoolnfo').hover(function(){
+    $('.Batool').toggleClass('animate');
+  });
+  $('.DarahInfo').hover(function(){
+    $('.Darah').toggleClass('animate');
+  });
 });
+
+
