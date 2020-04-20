5cd4bed04571292ea42b1a44b12d268b30c870ec
rowaidsayyed <rowaidsayyed@gmail.com>
2020-04-20 10:33:58 +0300

    test location

--- public/app.js
+++ public/app.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
   //   $('.dropdown-menu').toggleClass('hidden');
   //   $('.arrow').toggleClass('up');
   // });
+
+  // fetch('https://api.ipify.org/?format=json')
+  //   .then(result => result.json())
+  //   .then(x => console.log(x.ip));
   $('.dot').click(function () {
     $('.cont').toggleClass('showMenu');
     $('.pl2').toggleClass('pl3');
